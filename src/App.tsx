5a3e64a4b3694263ca734a7f953d824f2e0885ad
gscotav <gscotav@gmail.com>
2023-08-31 14:48:58 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code>  and save it y test HMR
+          Edit <code>src/App.tsx</code>  and save it for test HMR
         </p>
       </div>
       <p className="read-the-docs">
